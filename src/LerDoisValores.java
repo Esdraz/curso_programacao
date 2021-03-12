e759ceca05de28a71a9a5c3aeeb41298e617a8bd
Francisco Sousa <fsousa.jpa@gmail.com>
2021-03-12 15:50:10 -0300

    Initial commit

---
+++ src/LerDoisValores.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LerDoisValores {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int v1, v2, soma;
+
+        System.out.println("Entrada:");
+        v1 = sc.nextInt();
+        v2 = sc.nextInt();
+        soma = v1 + v2;
+
+        System.out.println("Saída:");
+        System.out.println("SOMA: " + soma);
+
+    }
+}
