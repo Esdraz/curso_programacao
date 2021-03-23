565e70665b375683917272a13dbdd72e3581a9f5
Francisco Sousa <fsousa.jpa@gmail.com>
2021-03-23 16:15:52 -0300

    update - mod 6 completo

--- src/lerCoordenadas.java
+++ src/lerCoordenadas.java
@@ -4,29 +4,22 @@ public class lerCoordenadas {
     public static void main(String[] Args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite Coordenada X: ");
-        double x = sc.nextDouble();
-        System.out.print("Digite Coordenada Y: ");
-        double y = sc.nextDouble();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
         while (x != 0 && y != 0) {
 
-
-            if (x >= 0 && y >= 0) {
+            if (x > 0 && y > 0) {
                 System.out.println("primeiro");
-
-            }
-            if (x <= 0 && y >= 0) {
+            } else if (x < 0 && y > 0) {
                 System.out.println("segundo");
-
-            }
-            if (x <= 0 && y <= 0) {
+            } else if (x < 0 && y < 0) {
                 System.out.println("terceiro");
-
-            } else {
+            } else if (x > 0 && y < 0) {
                 System.out.println("quarto");
-
             }
+            x = sc.nextInt();
+            y = sc.nextInt();
 
         }
         sc.close();
