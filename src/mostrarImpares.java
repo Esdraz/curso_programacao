749f26ab867e89eb5cc62229eddde21ba7ba4654
Francisco Sousa <fsousa.jpa@gmail.com>
2021-03-24 16:22:57 -0300

    update - exerc 3 mod 6

---
+++ src/mostrarImpares.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class mostrarImpares {
+    public static void main(String[] Args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+
+        for (int i = 1; i <= X; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
